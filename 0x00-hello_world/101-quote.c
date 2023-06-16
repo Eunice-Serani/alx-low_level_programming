b6f2876a30bfeba3048aae52f44193222e4b7804
Serani91 <eaamable91@gmail.com>
2023-06-15 18:08:23 -0700

    Print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 1(Success)
  */
-
 int main(void)
 
-}
-	write(2,
+{
+
+       write(2,
 
 	"and that piece of art is useful\" - Dora Kopar,2015-10-19\n",59);
 
-	return(1);
+       return (1);
 }
