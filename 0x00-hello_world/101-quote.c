5faf79e52d0b44dfad6c99edec9c7db89898125e
Serani91 <eaamable91@gmail.com>
2023-06-15 17:02:46 -0700

    Print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <unistdio.h>
-#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(2,n, strlen(n));
+	write(2,
 
 	"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n",59);
 
