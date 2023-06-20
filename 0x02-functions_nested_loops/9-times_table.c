b007244f364749083e6d1e0f93c05ace048829ed
Serani91 <eaamable91@gmail.com>
2023-06-20 10:08:50 -0700

    My functions and loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ _putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10)
-}
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
@@ -32,4 +32,3 @@ _putchar((k % 10) + '0');
 _putchar('\n');
 }
 }
-
