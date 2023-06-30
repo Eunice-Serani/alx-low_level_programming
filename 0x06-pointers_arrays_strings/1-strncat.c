5c5d170008b5891fa62765c624446a8ab557e74e
Serani91 <eaamable91@gmail.com>
2023-06-29 19:28:15 -0700

    More pointers,arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,18 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int length_of_string, z;
+/* length_of_string = length of destination of string */
 /* z is a counter for n bytes of src to be concatenated */
-/* length_of_string = length of destination string */
 
 	length_of_string = 0;
 	while (dest[length_of_string] != "\0")
 	{
 		length_of_string++;
 	}
-	while (z = 0; z < n && src[z] != "\0"; z++, length_of_string++)
+	while (z = 0; z < n && src[z] != "\0")
+	{
+		z++, length_of_string++
+	}
 	{
 	dest[length_of_string] = src[z];
 	}
