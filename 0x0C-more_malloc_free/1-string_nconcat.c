6976fd91bc31bfd60e927409e76f5129bcfaf78c
Serani91 <eaamable91@gmail.com>
2023-07-13 17:07:08 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append
  * @s2: string to concatenate
- * @n: numberof bytes from s2 to concatenate to s1
+ * @n: number of bytes from s2 to concatenate to s1
  *
  * Return pointer to the resulting string
  */
