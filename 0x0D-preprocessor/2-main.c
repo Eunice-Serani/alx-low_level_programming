d5bbb7f70e21df9b98e6646fb389be10c68eef2a
Serani91 <eaamable91@gmail.com>
2023-07-17 06:11:08 -0700

    Committing preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the name of the file
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s\n",_FILE_);
+	printf("%s\n", _FILE_);
 	return (0);
 }
