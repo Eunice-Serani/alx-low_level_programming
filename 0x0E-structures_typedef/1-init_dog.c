f0cc85a711f9e39ebe51dd341c80ce7d488e7e01
Serani91 <eaamable91@gmail.com>
2023-07-18 15:32:20 -0700

    C structures typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,16 +3,19 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
+ * struct - This is a struct
+ * @d: specific dog to initialize
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
