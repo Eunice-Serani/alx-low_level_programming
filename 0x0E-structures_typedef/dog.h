ef9b0d99367ae5aff9daaa6b5d690aaf4fde6497
Serani91 <eaamable91@gmail.com>
2023-07-18 16:23:35 -0700

    C structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,7 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
 
 void int_dog(struct dog *d, char *name, float age, char *owner);
