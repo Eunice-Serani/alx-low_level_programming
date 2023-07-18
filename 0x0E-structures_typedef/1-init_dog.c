303b1afec741d1e0f7fc1ba0c08403e0dba1e41b
Serani91 <eaamable91@gmail.com>
2023-07-18 16:49:58 -0700

    struct E init file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,12 +3,10 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * struct - This is a struct
  * @d: pointer to struct dog to initialize
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
- * Return:void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
