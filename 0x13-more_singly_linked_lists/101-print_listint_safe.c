01ae1314013655e9948fc28621d7c8ce3c9088b2
Serani91 <eaamable91@gmail.com>
2023-08-01 16:49:46 -0700

    Print list safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,36 +5,38 @@ size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of nodes in a looped list
- * print_listint_safe - Function that prints a linked list with loop safely
+ * size_t print_listint_safe - Function that prints a linked list
+ *  with a loop safely
  * @head: Points to the first node of a list
  *
  * Return: New_node in a list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+
 	const listint_t *new_temp_n = NULL;
 
-	const listint_t *new_l_n = NULL;
+	 const listint_t *new_l_n = NULL;
 
-	size_t new_node_counter = 0;
+	size_t new_node = 0;
 	size_t new_new_n;
 
 	new_temp_n = head;
 	while (new_temp_n)
 	{
 		printf("[%p] %d\n", (void *)new_temp_n, new_temp_n->n);
-		new_node_counter++;
+		new_node++;
 
 		new_temp_n = new_temp_n->next;
 		new_l_n = head;
 		new_new_n = 0;
 
-		while (new_new_n < new_node_counter)
+		while (new_new_n < new_node)
 		{
 			if (new_temp_n == new_l_n)
 			{
 				printf(" -> [%p] %d\n", (void *)new_temp_n, new_temp_n->n);
-				return (new_node_counter);
+				return (new_node);
 			}
 			new_l_n = new_l_n->next;
 			new_new_n++;
@@ -43,5 +45,5 @@ size_t print_listint_safe(const listint_t *head)
 		if (!head)
 			exit(98);
 	}
-	return (new_node_counter);
+	return (new_node);
 }
