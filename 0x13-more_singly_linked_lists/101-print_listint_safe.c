172ade5000c55ce1ebd75275c5bef6ea97425db0
Serani91 <eaamable91@gmail.com>
2023-08-02 09:12:52 -0700

    Print safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,30 +5,30 @@
  *  with a loop safely
  * @head: Points to the first node of a list
  *
- * Return: Number of nodes in a list
+ * Return: Number of new nodes in a list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *new_temp_n = NULL;
 	const listint_t *new_l_n = NULL;
 
-	size_t new_node = 0;
+	size_t new_curr = 0;
 	size_t new_new_n;
 
 	new_temp_n = head;
 	while (new_temp_n)
 	{
 		printf("[%p] %d\n", (void *)new_temp_n, new_temp_n->n);
-		new_node++;
+		new_curr++;
 		new_temp_n = new_temp_n->next;
 		new_l_n = head;
 		new_new_n = 0;
-		while (new_new_n < new_node)
+		while (new_new_n < new_curr)
 		{
 			if (new_temp_n == new_l_n)
 			{
 				printf(" -> [%p] %d\n", (void *)new_temp_n, new_temp_n->n);
-				return (new_node);
+				return (new_curr);
 			}
 			new_l_n = new_l_n->next;
 			new_new_n++;
@@ -37,5 +37,5 @@ size_t print_listint_safe(const listint_t *head)
 		if (!head)
 			exit(98);
 	}
-	return (new_node);
+	return (new_curr);
 }
