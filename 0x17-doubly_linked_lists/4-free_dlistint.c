fffa700b54b03a98033d61836f0b05eae2bd424e
Eunice-Serani <eaamable91@gmail.com>
2023-11-09 13:54:06 -0800

    0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dlistint - frees a list link
+ * @head: shows head of list that is to be freed
+ * Return: no return
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (!head)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
