fbf9764ad69e7798d051b7a705e2770f8be6976f
Eunice-Serani <eaamable91@gmail.com>
2023-11-14 10:01:40 +0000

    backup

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -3,10 +3,7 @@
 
 int printf(const char *format, ...)
 {
-	char numbers[] = "9 8 10 24 75 - 9\n";
-	char message[] = "Congratulations, you win the Jackpot!\n";
-
-	write(1, numbers, 17);
-	write(1, message, 38);
-	exit(0);
+	write(1, "9 8 10 24 75 +9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit (EXIT_SUCCESS);
 }
