30a32fcedbac8c7bcd6a987be0bcc3dc42befcef
Eunice-Serani <eaamable91@gmail.com>
2023-11-18 03:29:05 +0000

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Eunice-Serani/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/ ../libgiga.so"
+export LD_PRELOAD="$PWD/../libgiga.so"
